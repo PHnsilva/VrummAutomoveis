548bad586ed1fe8d566f945b4a3245acd9719591
phnsilva <phnsilva1@gmail.com>
2026-03-25 13:59:34 -0300

    refactor: migrate backend base to Micronaut structure

---
+++ backend/src/main/java/com/vrumm/vrummautomoveis/controller/AdminPedidoController.java
@@ -0,0 +1,1 @@
+package com.vrumm.vrummautomoveis.controller;
---
+++ backend/src/test/java/com/vrumm/VrummAutomoveisTest.java
@@ -0,0 +1,22 @@
+package com.vrumm;
+
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class VrummAutomoveisTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
